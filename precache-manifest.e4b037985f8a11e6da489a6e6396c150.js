8ce918e4f3b852d9cfd608c5fb5206f843278891
Cobraaz <anujbansal077@gmail.com>
2020-01-17 00:54:18 +0530

    Updates

---
+++ precache-manifest.e4b037985f8a11e6da489a6e6396c150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6194e820c163c3221c7ce8b266e7b6f2",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "246baa048af3e4212759",
+    "url": "/monster-rolodex/static/css/main.23f666fa.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monster-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "246baa048af3e4212759",
+    "url": "/monster-rolodex/static/js/main.844ab2da.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
